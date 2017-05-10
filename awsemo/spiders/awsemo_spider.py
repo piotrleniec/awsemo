8b1fe73612deb558d022538f4fbf3ead1814b81c
Marlena <bielat.marlena@gmail.com>
2017-05-10 21:46:02 +0200

    added changes after code review

--- awsemo/spiders/awsemo_spider.py
+++ awsemo/spiders/awsemo_spider.py
@@ -6,5 +6,5 @@ class AwsemoSpider(scrapy.Spider):
     start_urls = ['https://github.com/sindresorhus/awesome']
 
     def parse(self, response):
-        for link in response.css('div.readme a::attr(href)').extract():
-            self.log(link + '/n')
+        for url in response.css('#readme a::attr(href)').extract():
+            self.log(url + '/n')
